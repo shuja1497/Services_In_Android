1b1468135407dc2e5241c44b75c5f8c95a824551
shuja1497 <shujareshi1497@gmail.com>
2017-06-13 00:01:36 +0530

    using handler

--- app/src/main/java/shuja1497/joke/DelayedMessageService.java
+++ app/src/main/java/shuja1497/joke/DelayedMessageService.java
@@ -3,14 +3,29 @@ package shuja1497.joke;
 import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.Toast;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 public class DelayedMessageService extends IntentService {
 
     public static final String EXTRA_MESSAGE = "message";
 
+    private Handler handler;
+
+/*
+    This method runs on the main thread, so it
+    creates a new handler on the main thread.*/
+    @Override
+    public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
+        handler = new Handler();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
     public DelayedMessageService() {
+
         super("DelayedMessageService");
     }
 
@@ -19,7 +34,7 @@ public class DelayedMessageService extends IntentService {
 
         synchronized (this) {
             try {
-                wait(10000);
+                wait(5000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -30,7 +45,12 @@ public class DelayedMessageService extends IntentService {
     }
 
     private void showText(final String text) {
-        Log.v("DelayedMessageService", "The message is: " + text);
+        //Log.v("DelayedMessageService", "The message is: " + text);
+        handler.post(new Runnable() {
+            public void run() {
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
